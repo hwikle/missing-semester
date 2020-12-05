178313b312f192fe28cb6cff6940d5386ce2d2eb
Hank Wikle <henry.wikle@gmail.com>
2020-12-05 16:48:29 -0700

    Add solution to lecture 2, exercise 5

--- shell-tools/solutions.sh
+++ shell-tools/solutions.sh
@@ -11,3 +11,6 @@ ls -lah --color
 
 # Exercise 4
 find . -name "*.html" | xargs -d "\n" zip html.zip
+
+# Exercise 5
+find . -type f -printf "%TY-%Tm-%Td %TT %p\n" | sort -rn | head -n1 | cut -f3- -d" "
